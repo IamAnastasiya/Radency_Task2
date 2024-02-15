2af7c4af74f3a57f35d36d6d7daf067f7b32e871
Anastasiia Balabukha <balabukha.anastasiya@gmail.com>
2024-02-15 17:44:00 +0200

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,44 @@
-import Table from './components/ui/Table';
-import { useState, useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import  { noteActions }  from './store/notes-slice';
-import  { modalActions }  from './store/modal-slice';
+import Table from "./components/ui/Table";
+import { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { noteActions } from "./store/notes-slice";
+import { modalActions } from "./store/modal-slice";
 
-import classes from './App.module.css';
-import ModalContainer from './components/modal/ModalContainer';
-
+import classes from "./App.module.css";
+import ModalContainer from "./components/modal/ModalContainer";
 
 function App() {
+    const [isArchiveVisible, setIsArchiveVisible] = useState(false);
 
-  const [isArchiveVisible, setIsArchiveVisible] = useState(false);
-  const shoudGetCategoryCounts = useRef(true);      // to prevent duplicated useEffect running for the initial render
+    const dispatch = useDispatch();
+    const archiveBtnText = isArchiveVisible ? "Hide Archive" : "Show Archive";
 
-  const dispatch = useDispatch();
-  const archiveBtnText = isArchiveVisible ? 'Hide Archive' : 'Show Archive';
+    useEffect(() => {
+        dispatch(noteActions.getCategoryCounts());
+    }, []);
 
-  useEffect(() => {
-    if (shoudGetCategoryCounts.current) { 
-      shoudGetCategoryCounts.current = false;
-      dispatch(noteActions.getCategoryCounts());
-    } 
-  }, []);
+    const openModalHandler = () => {
+        dispatch(modalActions.toggleVisibility());
+    };
 
+    const archiveVisibilityHandler = () => {
+        setIsArchiveVisible((prevState) => (prevState = !prevState));
+    };
 
-  const openModalHandler = () => {
-    dispatch(modalActions.toggleVisibility());
-  }
-
-  const archiveVisibilityHandler = () => {
-    setIsArchiveVisible((prevState) => prevState = !prevState);
-  }
-
-  return (
-    <div className={classes.wrapper}>
-      <ModalContainer />
-      <Table tableMode={'active'}/>
-      <button className={classes['action-create']} onClick={openModalHandler}>Create note</button>
-      <Table tableMode={'summary'}/>
-      <button className={classes['archive-toggler']} onClick={archiveVisibilityHandler}>{archiveBtnText}</button>
-      {isArchiveVisible && <Table tableMode={'archive'}/>}
-    </div>
-  );
+    return (
+        <div className={classes.wrapper}>
+            <ModalContainer />
+            <Table tableMode={"active"} />
+            <button className={classes["action-create"]} onClick={openModalHandler}>
+                Create note
+            </button>
+            <Table tableMode={"summary"} />
+            <button className={classes["archive-toggler"]} onClick={archiveVisibilityHandler}>
+                {archiveBtnText}
+            </button>
+            {isArchiveVisible && <Table tableMode={"archive"} />}
+        </div>
+    );
 }
 
 export default App;
